9655f3ca022d35a30d8f77a83fe1e027764ca6f7
86131 <760856329@qq.com>
2020-09-06 15:25:47 +0800

    重命名

--- git.py
+++ git.py
@@ -1,3 +1,16 @@
+'''
+先自己注册github账号，本地安装git环境，打开Pycharm-File-Settings-Version control-Git设置页面
+填写git在本地的地址
+
+pull拉取代码
+找到VCS-Checkoutfrom Version Control-Git
+输入代码仓库地址
+
+打开Vcs-Git-Commit提交代码
+
+打开Vcs-Git-Push，勾选push标签为当前分支
+'''
+
 a = "abcdef"
 
 # 以 str 为分隔符切片 mystr，如果 maxsplit有指定值，则仅分隔 maxsplit 个子字符串
